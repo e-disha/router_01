7f6df720a684dcfa1c55762b38926d829f7af757
disha e <edisha@smartict.al>
2023-04-02 14:52:36 +0200

    added background image

--- src/About.js
+++ src/About.js
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div className="about">
             <h2>About us</h2>
-            <p>This is a mere though experiment, try to make it more fun by imagining this scenario.</p>
+            <p>This is a mere though experiment, try to make it more fun by imagining the scenarios.</p>
         </div>
     );
 }
